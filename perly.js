929e555f35649e2ead8a5469ad438bea17e64393
Jon Gentle <atrodo@atrodo.org>
2016-12-05 22:22:44 -0500

    Inital code check point with just-starting-to-work gmr

---
+++ perly.js
@@ -0,0 +1,18 @@
+var grammar = require('./gmr.js');
+
+var parser_gen = require('./lib/parser');
+var parser = parser_gen('grammar', grammar);
+
+if (require.main === module)
+{
+  var args = process.argv.slice(1);
+  if (args[1] == null)
+  {
+    console.log('Usage: '+args[0]+' FILE');
+    process.exit(1);
+  }
+
+  var path = require('path').normalize(args[1]);
+  var source = require('fs').readFileSync(path, "utf8");
+  console.log(parser.parse(source));
+}
---
+++ perly2gmr.pl
@@ -0,0 +1,218 @@
+#!/usr/bin/env perl
+
+use v5.18;
+
+my %tokens;
+my @order;
+
+my $rule_re = qr<
+  ( (?:[^{] | '{ | {')*? )
+  \s*
+  (/[*] .*? [*]/)?
+  \s*
+  (?: { (.*) } )?
+  \s*
+  ;?
+  \s*
+  $
+  >xms;
+
+while (<>)
+{
+  chomp;
+  if (m/%start \s grammar/xms .. m/^%%/xms)
+  {
+    if (m/^%(token | type | nonassoc | left | right)/xms)
+    {
+      my $type = $1;
+      my @syms = split;
+
+      shift @syms;
+      shift @syms
+        if $syms[0] =~ m/<\w+>/xms;
+      
+      foreach my $sym (@syms)
+      {
+        $tokens{$sym} = { type => $type, sym => $sym, };
+        push @order, $sym
+          if $type eq 'token';
+      }
+    }
+  }
+
+  if (my $seq = m/^\w+ \s* :/xms .. m{^(?: [/][*] | (?:\t|[ ]{,8}); )}xms)
+  {
+    state $rule = '';
+    if ($seq == 1)
+    {
+      #say $rule;
+      $rule = '';
+    }
+
+    $rule .= "$_\n";
+
+    if ($seq =~ m/E0$/xms)
+    {
+      my ($sym, $rules) = $rule =~ m/^(\w+) \s* : (.*)/xms;
+      my $token = { type => 'nonterm', sym => $sym };
+      $tokens{$sym} = $token;
+      push @order, $sym;
+
+      #map { my %a = ( line => $_ ); @a{qw/raw_rule comment code/} = $_ =~ m/ $rule_re /xms; \%a }
+      
+      my @rules =
+        map { { line => $_, raw_rule => '', comment => '', code => '', } }
+        map { $_ =~ s/\s+/ /xmsg; $_ }
+        map { $_ =~ s/; \s* $//xmsg; $_ }
+        split(m/^(?:\t|\s{,8})[|]/xms, $rules);
+
+      foreach my $rule ( @rules )
+      {
+        my @stack;
+        my @line = split //, $rule->{line};
+        for (my $i; $i < $#line; $i++)
+        {
+          my $chr = $line[$i];
+          if ( @stack )
+          {
+            if ( $stack[0] eq 'comment')
+            {
+              $rule->{comment} .= $chr;
+              if ( $chr eq '*' && $line[$i+1] eq '/')
+              {
+                $rule->{comment} .= '/';
+                $i++;
+                pop @stack;
+              }
+              next;
+            }
+
+            if ( $stack[0] eq 'code')
+            {
+              $rule->{code} .= $chr;
+              if ($chr eq q[\\])
+              {
+                $rule->{code} .= $line[$i+1];
+                $i++;
+                next;
+              }
+
+              if ( @stack == 1 && $chr eq '}')
+              {
+                $rule->{code} .= ' ';
+                pop @stack;
+              }
+              elsif ( $stack[-1] eq $chr )
+              {
+                pop @stack;
+              }
+              elsif ( $chr eq q['] )
+              {
+                push @stack, q['];
+              }
+              elsif ( $chr eq q["] )
+              {
+                push @stack, q["];
+              }
+              elsif ( $chr eq q[{] )
+              {
+                push @stack, q[}];
+              }
+              next;
+            }
+          }
+
+          if ($chr eq '/' && $line[$i+1] eq '*')
+          {
+            push @stack, 'comment';
+            $rule->{comment} .= '/*';
+            $i++;
+            next;
+          }
+
+          if ($chr eq '{' )
+          {
+            push @stack, 'code';
+            $rule->{code} .= '{';
+            next;
+          }
+
+          $rule->{raw_rule} .= $chr;
+        }
+
+        my @terms = split m/\s+/, $rule->{raw_rule};
+        #warn Dumper(\@terms);
+        my $result = [];
+
+        while (@terms)
+        {
+          my $term = shift @terms;
+
+          if ($term eq '')
+          {
+          }
+          elsif ($term =~ m/^% (.*) $/xms)
+          {
+            push @$result, "{$1 " . shift(@terms) . "}";
+          }
+          elsif ( $term =~ m/(['"])(.*)\1/xms)
+          {
+            push @$result, $2;
+          }
+          elsif (exists $tokens{$term})
+          {
+            push @$result, "<$term>";
+          }
+          else
+          {
+            push @$result, "$term";
+          }
+        }
+        $rule->{rule} = join(" ", @$result);
+      }
+
+      #say Dumper(\@rules);
+      #say $rules;
+      $token->{rules} = [ @rules ];
+    }
+  }
+};
+
+use Data::Dumper;
+#warn Dumper(@order);
+
+say "grammar = {";
+foreach my $sym (@order)
+{
+  local $Data::Dumper::Indent = 1;
+  local $Data::Dumper::Varname = $sym;
+  local $Data::Dumper::Sortkeys = 1;
+
+  my $re = $sym;
+  $re =~ s/^'(.*)'$/$1/g;
+  $re =~ s/([\^*+?.()[\]{}$ ])/[$1]/xmsg;
+  my $token = $tokens{$sym};
+  my @rules = ( "/$re/i" );
+
+  @rules = @{ $token->{rules} }
+    if defined $token->{rules};
+
+  my $dump = Dumper($token);
+  $dump =~ s[[*]/][\\*\\/]xmsg;
+  $dump =~ s/^/    /gxms;
+  $dump =~ s[\A][  /*\n]xms;
+  $dump =~ s[\Z][\n  */]xms;
+
+  say $dump;
+  say sprintf(qq[  %-16s : \[], qq["$sym"]);
+
+  foreach my $rule ( @rules )
+  {
+    say qq[\t\t       ] . (ref $rule ? qq["$rule->{rule}"] : $rule) . qq[,];
+  }
+
+  say "\t\t     ],";
+}
+
+say "};";
+say "\nmodule.exports = grammar";
