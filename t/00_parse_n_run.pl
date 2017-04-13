d62ab10a0ab2195aa3d149865897d9a486ed6921
Jon Gentle <atrodo@atrodo.org>
2017-04-12 23:57:17 -0400

    Our first passing test

--- t/00_parse_n_run.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env perly
-say 'ok 2 - multi-line', 'ok 3 - multi-line electric boogaloo', '# comment';
-say 'ok 1 - output happens';
-say '3..3';
---
+++ t/00_parse_n_run.t
@@ -0,0 +1,4 @@
+#!./perly.js
+say 'ok 1 - output happens';
+say 'ok 2 - multi-line', 'ok 3 - multi-line electric boogaloo', '# comment';
+say '1..3';
